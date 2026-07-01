05179e39a10253ff31cf064459a24c23d9586509
lsiyan609-droid <lsiyan609@gmail.com>
2026-07-01 20:42:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ if (btn && menu) {
 }
 
 // 切换语言（目前先测试）
-function setLanguage(lang) {
+async function setLanguage(lang) {
 
     const names = {
         zh: "中文",
@@ -36,8 +36,29 @@ function setLanguage(lang) {
     };
 
     btn.innerHTML = `🌐 ${names[lang]} ▼`;
-
     menu.style.display = "none";
 
-    console.log("切换到：" + lang);
+    // 读取对应语言文件
+    const response = await fetch(`lang/${lang}.json`);
+    const data = await response.json();
+
+    // 替换所有带 data-i18n 的文字
+    document.querySelectorAll("[data-i18n]").forEach(item => {
+        const key = item.dataset.i18n;
+
+        if (data[key]) {
+            item.textContent = data[key];
+        }
+    });
+
+    // 记住用户选择
+    localStorage.setItem("language", lang);
 }
+
+window.addEventListener("load", async () => {
+
+    const lang = localStorage.getItem("language") || "zh";
+
+    await setLanguage(lang);
+
+});
